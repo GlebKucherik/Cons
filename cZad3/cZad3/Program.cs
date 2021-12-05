2da84124d7cb31388b685bb3a8c709c83b423b13
GlebKucherik <95340898+GlebKucherik@users.noreply.github.com>
2021-12-05 14:13:41 +0300

    Add files via upload

---
+++ cZad3/cZad3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace cZad3
+{
+    class Program
+    {
+        static double f(double a, double b)
+        {
+            return Math.Sqrt(a * a + b * b);
+        }
+        static string Result(double ab, double ac, double cd)
+        {
+            string str = (ab + ac + cd + f(f(ab, ac), cd)).ToString();
+            return str;
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите AB: ");
+                double AB = Double.Parse(Console.ReadLine());
+                Console.Write("Введите AC: ");
+                double AC = Double.Parse(Console.ReadLine());
+                Console.Write("Введите DC: ");
+                double DC = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Результат: {0}", Result(AB, AC, DC));
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Вы ввели что-то не то!");
+            }
+        }
+    }
+}   
---
+++ cZad3_2/cZad3_2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace cZad3_2
+{
+    class Program
+    {
+        static double f(double x)
+        {
+            if (x == 1 || x == -1)
+            {
+                return 1;
+            }
+            else if (x >= 0 && x != 1)
+            {
+                return -1 / (1 - x);
+            }
+            else
+            {
+                return 1 / (1 + x);
+            }
+        }
+        static void f(double x, out double y)
+        {
+            if (x == 1 || x == -1)
+            {
+                y = 1;
+            }
+            else if (x >= 0 && x != 1)
+            {
+                y = -1 / (1 - x);
+            }
+            else
+            {
+                y = 1 / (1 + x);
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                string zad2 = String.Empty;
+                string zad3 = String.Empty;
+                Console.Write("Введите a:");
+                double a = Double.Parse(Console.ReadLine());
+                Console.Write("Введите b:");
+                double b = Double.Parse(Console.ReadLine());
+                Console.Write("Введите h:");
+                double h = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Задание 2");
+                for (double x = a; x<=b; x+=h)
+                {
+                    zad2 += f(x).ToString() + " ";
+                }
+                Console.WriteLine(zad2);
+                Console.WriteLine("Задание 3");
+                for (double x = a; x <= b; x += h)
+                {
+                    double y;
+                    f(x, out y);
+                    zad3 += y + " ";
+                }
+                Console.WriteLine(zad3);
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Вы ввели что-то не то!");
+            }
+        }
+    }
+}
