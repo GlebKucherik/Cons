9fa0a2dd25d376292d68a6a41d72d9598666b33a
GlebKucherik <95340898+GlebKucherik@users.noreply.github.com>
2021-12-07 23:23:12 +0300

    Add files via upload

---
+++ cZad4_1/cZad4_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace cZad4_1
+{
+    class prac
+
+    {
+        public prac(int a, int b)
+        {           
+            Console.WriteLine(nod(Math.Abs(a), Math.Abs(b)));           
+        }
+        int nod(int a, int b)
+        {
+            if (a == b) 
+            { 
+                return a; 
+            }
+            else if (a > b)
+            {
+                return nod(a - b, b);
+            }
+            else
+            {
+                return nod(a, b - a);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите a: ");
+                int a = int.Parse(Console.ReadLine());
+                Console.Write("Введите b: ");
+                int b = int.Parse(Console.ReadLine());
+                prac p = new prac(a, b);
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Нужно вводить целые числа!");
+            }
+        }
+    }
+}
---
+++ cZad4_2/cZad4_2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace cZad4_2
+{
+    class prac
+    {
+        public prac(int k)
+        {
+            rec(k, 0);
+        }
+        void str(int i, string c)
+        {
+            int j = 0;
+            while (j < i)
+            {
+                j++;
+                Console.Write(c);
+            }
+        }
+        void rec(int n, int i)
+        {
+            if (n >= 0)
+            {
+                str(i, "*");
+                str(n, " ");
+                str(i, "*");
+                Console.WriteLine();
+                rec(n - 2, i + 1);
+                str(i, "*");
+                str(n, " ");
+                str(i, "*");
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите n: ");
+                int n = int.Parse(Console.ReadLine());
+                if (n%2!=0)
+                {
+                    throw new Exception("Число должно быть четное!");
+                }
+                prac p = new prac(n);
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Вы должны ввести целое число!");
+            }
+            catch(Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+            }
+        }
+    }
+}
